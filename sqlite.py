0ead2ebda49c7e501eb5bcd1ba52708b6dc73f4c
Neha Mandwal <nehamandwal123@gmail.com>
2025-07-24 18:25:08 +0530

    Add files via upload

---
+++ sqlite.py
@@ -0,0 +1,32 @@
+import sqlite3
+#write code to connect to sqlite 3
+connection=sqlite3.connect("student.db")
+
+cursor=connection.cursor()
+
+
+table_info = """
+CREATE TABLE IF NOT EXISTS STUDENT (
+    NAME VARCHAR(25),
+    CLASS VARCHAR(25),
+    SECTION VARCHAR(25),
+    MARKS INT
+)
+"""
+
+
+cursor.execute(table_info)
+
+cursor.execute('''Insert Into STUDENT values('Krish','Data Science','A',90)''')
+cursor.execute('''Insert Into STUDENT values('John','Data Science','B',100)''')
+cursor.execute('''Insert Into STUDENT values('Mukesh','Data Science','A',86)''')
+cursor.execute('''Insert Into STUDENT values('Jacob','DEVOPS','A',50)''')
+cursor.execute('''Insert Into STUDENT values('Dipesh','DEVOPS','A',35)''')
+
+#display all records
+print("the inserted records are")
+data=cursor.execute('''Select* from STUDENT''')
+for row in data:
+    print(row)
+connection.commit()
+connection.close()
